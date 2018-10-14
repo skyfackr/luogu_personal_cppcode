9560a6566303e8bd95093e8a4eeaae4ec6643073
skyfackr <44115200+skyfackr@users.noreply.github.com>
2018-10-14 16:07:26 +0800

    模板 快速幂
    可用
    基于二进制

--- luogu_1226（模板_快速幂）/luogu_1226（模板_快速幂）.cpp
+++ luogu_1226（模板_快速幂）/luogu_1226（模板_快速幂）.cpp
@@ -1,33 +1,25 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
-ll b,p,k,d,dd,pp;
+ll b,p,k,ans,dd,pp;
 int main()
 {
 	cin>>b>>p>>k;
-	d=0;
+	ans=1;
 	dd=b;
 	pp=p;
-	if (p%2==1)
+	while (p>0)
 	{
-		p--;
-		d=1;
-		
-	}
-	while (p>1)
-	{
-		if (p%2==1)
-	{
-		p--;
-		b*=dd;
-		
-	}
-		b=pow(b,2);
+		if (p&1)
+		{
+			ans*=b;
+			ans%=k;
+		}
+		b*=b;
 		b%=k;
-		
 		p/=2;
 	}
 //	if (d==1) b*=dd;
-	cout<<dd<<"^"<<pp<<" mod "<<k<<"="<<b*dd%k<<endl;
+	cout<<dd<<"^"<<pp<<" mod "<<k<<"="<<ans%k<<endl;
 	return 0;
 }
