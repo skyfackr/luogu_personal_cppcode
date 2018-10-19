54cdc2063faa3df9abc9399a6166f726db22a95e
skyfackr <44115200+skyfackr@users.noreply.github.com>
2018-10-20 00:27:34 +0800

    20181020

--- luogu_1008/luogu_1008.cpp
+++ luogu_1008/luogu_1008.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int i1,i2,i3,i4,i5,i6,i7,i8,i9,j1,j2,j3;
+int i1,i2,i3,i4,i5,i6,i7,i8,i9;
+int jj1,jj2,jj3;
+ofstream f;
 int main()
 {
+	f.open("ans.txt",ios::out);
 	for (i1=1;i1<=9;i1++)
 		for (i2=1;i2<=9;i2++)
 			for (i3=1;i3<=9;i3++)
@@ -13,6 +16,17 @@ int main()
 								for (i8=1;i8<=9;i8++)
 									for (i9=1;i9<=9;i9++)
 									{
-										if ()
+										if (i1!=i2&&i1!=i3&&i1!=i4&&i1!=i5&&i1!=i6&&i1!=i7&&i1!=i8&&i1!=i9&&i2!=i3&&i2!=i4&&i2!=i5&&i2!=i6&&i2!=i7&&i2!=i8&&i2!=i9&&i3!=i4&&i3!=i5&&i3!=i6&&i3!=i7&&i3!=i8&&i3!=i9&&i4!=i5&&i4!=i6&&i4!=i7&&i4!=i8&&i4!=i9&&i5!=i6&&i5!=i7&&i5!=i8&&i5!=i9&&i6!=i7&&i6!=i8&&i6!=i9&&i7!=i8&&i7!=i9&&i8!=i9)
+										{
+											jj1=i1*100+i2*10+i3;
+											jj2=i4*100+i5*10+i6;
+											jj3=i7*100+i8*10+i9;
+											if (jj1*6==jj2*3&&jj2*3==jj3*2)
+											{
+												f<<jj1<<" "<<jj2<<" "<<jj3<<endl;
+											}
+										}
 									}
+	f.close();
+	return 0;
 }
