cb9c2fd49ff5aad4f0baee2e70504ac45c0d3aae
skyfackr <44115200+skyfackr@users.noreply.github.com>
2018-11-17 01:57:47 +0800

    20181116

---
+++ luogu_U51941/luogu_U51941.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+char ch;
+long long ans;
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	while (cin>>ch) ans++;
+	cout<<ans<<endl;
+	return 0;
+}
